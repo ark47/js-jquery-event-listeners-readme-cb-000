bee1a554ffc46fb10c12e747c4f4e85a533f3aed
Chris Nunez <cenunez47@gmail.com>
2017-10-13 01:23:53 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,13 +10,13 @@ function frameIt() {
   });
 }
 
-function submitIt() {}
-
-function pressIt() {
+function submitIt() {
   $('form').on("submit", function() {
     alert("Your form is going to be submitted now.");
   });
 }
+
+function pressIt() {}
 
 $(document).ready(function(){
 
